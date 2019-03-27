99efa05ac6ab8d0c264ff26b9ab44681a3ae2c26
Jairo Fernandez <jairog12@outlook.com>
2019-03-26 23:43:46 -0500

    add configurations for new gems

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # README
 
 List of gems
-- gem install rspec
+- gem install rspec-rails
+https://github.com/rspec/rspec-rails
+
+
 - gem install shoulda-matchers
+https://github.com/thoughtbot/shoulda-matchers
+
 - gem install factory-bot
+https://www.rubydoc.info/gems/factory_bot/file/GETTING_STARTED.md
+
 - gem install database_cleaner
+https://github.com/DatabaseCleaner/database_cleaner
+
 - gem install faker
 
 rails generate rspec:install
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,15 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    with.library :rails
+  end
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -31,6 +39,18 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
